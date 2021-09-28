5d010298a631c9ee01cc025efa5fc57c315db7a7
Nick Versteeg <nick@terraindex.com>
2021-09-28 11:03:49 +0200

    Added store

---
+++ memorycard/src/stores/store.ts
@@ -0,0 +1,33 @@
+import { reactive, readonly } from 'vue';
+
+export abstract class Store<T extends Record<string, unknown>> {
+  protected state: T;
+
+  protected defaultState: T;
+
+  constructor() {
+    const data = this.data();
+    this.setup(data);
+    this.state = reactive(data) as T;
+    this.defaultState = JSON.parse(JSON.stringify(this.state));
+  }
+
+  protected abstract data(): T;
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+  protected setup(data: T): void { }
+
+  public getState(): T {
+    // This prevents devs to update the state directly and makes sure that they will need to use an action
+    return readonly(this.state) as T;
+  }
+
+  // Checks if the application is running online or offline
+  public isOnline(): boolean {
+    return navigator.onLine;
+  }
+
+  public resetStore() {
+    this.state = JSON.parse(JSON.stringify(this.defaultState));
+  }
+}
