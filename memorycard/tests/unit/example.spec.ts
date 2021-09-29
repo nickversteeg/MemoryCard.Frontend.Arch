63b1b4ccaee0bf3976080743ee19068f477261e2
Nick Versteeg <nick@terraindex.com>
2021-09-29 14:19:44 +0200

    Added example tests

--- memorycard/tests/unit/example.spec.ts
+++ memorycard/tests/unit/example.spec.ts
@@ -1,12 +1,38 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import "reflect-metadata";
+import { container } from "tsyringe";
+import GameService from "@/services/game-service";
+import GameStore from "@/stores/game-store";
+import GameDataHandler from "@/dataHandlers/game-data-handler";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+let games = [];
+
+beforeAll()
+
+describe("game-service.ts", () => {
+  it("gets games from the API", async () => {
+    // arrange
+    const gameService = container.resolve(GameService);
+
+    // act
+    const games = await gameService.getGamesAsync();
+
+    // assert
+    expect(games.length).toEqual(12);
+  });
+  it("marks a game as selected", async () => {
+    // arrange
+    const gameStore = container.resolve(GameStore);
+    const gameService = container.resolve(GameService);
+    const gameDataHandler = container.resolve(GameDataHandler);
+
+    const spy = jest.spyOn(gameDataHandler, "setSelectedGame");
+
+    // act
+    const games = await gameService.getGamesAsync();
+    gameDataHandler.setSelectedGame(games[0].id);
+
+    // assert
+    expect(spy).toBeCalledTimes(1);
+    expect(gameStore.getState().selected.id).toEqual(games[0].id);
   });
 });
